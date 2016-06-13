b220c7cd44811bd6eebaac50adb490aff5c306b7
Jhordanny Hernandez <j.jhordanny18@gmail.com>
2016-06-13 13:23:34 -0400

    Banner del grupo de Facebook ¨Azur {Open Source};¨

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var strFBGraphURL = "https://graph.facebook.com/";
 var strAccessToken = "239199982824315|3L1T4SUoHU7KPXUB3UiEhPnsTns";
-var strGroupId = "206265449388631";
+var strGroupId = "864827726949364";
 var strUrl = strFBGraphURL + strGroupId + "/members?access_token=" + strAccessToken;
 $.getJSON( strUrl, function( listMembers ) {
   var strImg = "";
